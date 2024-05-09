804730c01789ea96cc2e57927f16297bc14e8503
Winstonmac <168591591+Winstonmac@users.noreply.github.com>
2024-05-09 13:55:39 +0100

    Update task_one.py

--- task_one.py
+++ task_one.py
@@ -1,18 +1,18 @@
-def plot_data(my_file):
-    '''
-    This function plots a scatterplot provided with a coordinate text file path 
+import pandas as pd
+import matplotlib.pyplot as plt
 
-    Parameters
-    ----------
-    my_file : STRING 
-        Required path to the file with coordinates in text format 
+# Load the data from the TXT file, assuming it is comma-separated
+# Add 'header=None' if your data does not include headers
+# and specify column names manually
+data = pd.read_csv('x_y_coordinates.txt', delimiter=',', header=None, names=['x', 'y'])
 
-    Returns
-    ---------
-    Scatterplot 
-    '''
-    scatter_plot = None 
-    """
-    ==> Write your code here <==
-    """
-    return scatter_plot 
+# Check the column names
+print("Column names in the dataset:", data.columns)
+
+# Create a scatter plot using the correct column names
+plt.scatter(data['x'], data['y'])
+plt.title('Scatter Plot of Coordinates')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+plt.show()
